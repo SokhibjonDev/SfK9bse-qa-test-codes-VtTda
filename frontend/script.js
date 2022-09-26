c5a4d1b75fde21f1ed8bf6f9b49470219bfad6cb
SokhibjonDev <110424000+SokhibjonDev@users.noreply.github.com>
2022-09-26 14:09:40 +0500

    feat : oop , prototype

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,86 @@
-console.log('Hello world');
 // Theme : Object-oriented-programming ( OOP )
+// class User {
+//     constructor(name, surname){
+//         this.name = name
+//         this.surname = surname
+//     }
+// }
+
+// const username = new User ('Steve', 'Job')
+// console.log(username.name, username.surname);
+
+// class.constructor function
+
+
 
 // Theme : Async / Await
 
 // Theme : Functions
 
+// declarative
+// Name(firstName = 'Sokhibjon', lastName = 'Soliyev')
+
+// function Name(firstName, lastName) {
+//     console.log(`Hi , I'm ${lastName} ${firstName}`);
+// }
+
+// expression
+
+// Function() with variable Let
+
+// let username = function () {
+//     console.log('Hello my name`s Gustavo , but you can call me Gus');
+// }
+// username()
+
+// global alert function
+
+
+// function MyFunction(username) {
+//     let num = 10
+//     num++
+//     console.log(num);
+//     console.log(`Hi , I'm ${username}`);
+// }
+
+// MyFunction(username = 'John')
+
+// function MyFunction(username) {
+//     for (let i = 0; i <= 10; i++) {
+//         console.log(username, 'age :', i);
+//     }
+// }
+// MyFunction(username='Tom`s')
+
 // Theme : Prototype
+
+// Prototype function (){}
+
+// function Person(firstName, lastName, age, job, nation){
+//     this.firstName = firstName
+//     this.lastName = lastName
+//     this.age = age
+//     this.job = job
+//     this.nation = nation
+// }
+
+// const myTeacher = new Person('Steve', 'Jobs', 50, 'Developer', 'German')
+// const myStudent = new Person('Mark', 'Zuckerberg', 30, 'Engineer', 'French')
+
+// console.log(myTeacher, myStudent);
+
+// function Person(first, last, age, eyecolor) {
+//     this.firstName = first
+//     this.lastName = last
+//     this.age = age
+//     this.eyeColor = eyecolor
+// }
+
+
+// const myStudent1 = new Person('Elon', 'Musk', 52, 'brown')
+// const myStudent2 = new Person('Mark', 'Zuckerberg', 30, 'blue')
+// const myStudent3 = new Person('Steve', 'Jobs', 60, 'green')
+
+// console.log(myStudent1, myStudent2, myStudent3);
 
 // Theme : DRY && SOLID
--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState();
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  function decrement() {
+    setCounter(counter - 1);
+  }
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Counter :{counter}</h1>
+      <button onClick={increment} className="btn">
+        plus
+      </button>
+      <button onClick={decrement} className="btn">
+        minus
+      </button>
     </div>
   );
 }
